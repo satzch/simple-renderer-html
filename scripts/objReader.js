93940239d510cd89b3b39e29364d8de81287a9a5
satzch <gamingsplay240@gmail.com>
2025-01-01 20:07:58 +0530

    refactor: use mesh class to generate mesh for .obj

--- scripts/objReader.js
+++ scripts/objReader.js
@@ -15,33 +15,26 @@ fileInput.addEventListener("change", async () => {
     if (file) {
         objectData = await file.text();
     }
-    // console.log(objectData.split("\n"))
+    
     if (objectData) {
         objectData = objectData.split("\n");
         for (let line of objectData) {
             if (line[0] == 'v') {
                 line = line.split(" ");
-                let vertex = [];
-                for (let i = 1; i < 4; i++) {
-                    vertex.push(Number(line[i]));
-                }
-                // console.log("Vertex: ", vertex)
-                selectedObj.vertices.push(vertex);
+                selectedObj.vertices.push(new Vertex(Number(line[1]), Number(line[2]), Number(line[3])));
             }
 
             if (line[0] == 'f') {
                 line = line.split(" ");
-                let triangle = [];
-                for (let i = 1; i < 4; i++) {
-                    triangle.push(Number(line[i]) - 1);
-                }
-                // console.log("Faces: ", triangle)
-                selectedObj.tries.push(triangle);
+                selectedObj.tries.push(line[1] - 1);
+                selectedObj.tries.push(line[2] - 1);
+                selectedObj.tries.push(line[3] - 1);
             }
         }
     }
 
 
-    obj = selectedObj;
+    obj = new Mesh(selectedObj.vertices, selectedObj.tries);
+    obj.setPosition(new Vector(0, 0, -10));
 })
 
